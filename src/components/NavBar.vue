bebabbf325d8a27bd621dd7984003eba6237d62b
Anna93803 <anna.tassone93@gmail.com>
2022-06-24 14:41:44 +0200

    add NavBar Component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="bg-navbar">
+        <div class="logo">
+            <img src="../assets/img/logo-small.svg" alt="logo-spotify">
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'NavBar',
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/variables.scss';
+
+.bg-navbar {
+    background-color:  $ColorSecondary;
+    padding: .5rem;
+
+    .logo {
+        width: 45px;
+
+        img {
+            width: 100%;
+        }
+    }
+}
+</style>
